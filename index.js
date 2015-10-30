0606e69f74ac890390f7b7a6dccbe7206139a345
fionaleeeee <FionaLee@FionaLees-MBP.wv.cc.cmu.edu>
2015-10-29 20:01:59 -0400

    yeahhhhh

---
+++ index.js
@@ -0,0 +1,13 @@
+// function fib(n){
+//     return 5;
+// }
+
+// var x = document.getElementsByClassName("summary-blk")
+
+
+function whenPageReady(){
+    $(".button-collapse").sideNav();
+}
+
+
+$(document).ready(whenPageReady)
